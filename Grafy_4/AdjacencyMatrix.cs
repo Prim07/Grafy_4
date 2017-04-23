c788366f5db6cd8d1ae706900067fd4b6fefc4c4
Artur Kawala <kawala.artek@gmail.com>
2017-04-24 01:15:29 +0200

    Losowanie digrafu G(n,p). Rysowanie digrafu wraz ze strzałkami.

---
+++ Grafy_4/AdjacencyMatrix.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Grafy_4
+{
+    class AdjacencyMatrix
+    {
+        // Brzydkoszybka enkapsulacja
+        public int[,] AdjacencyArray;
+
+        // Konstruktor
+        public AdjacencyMatrix(int n)
+        {
+            AdjacencyArray = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    AdjacencyArray[i, j] = 0;
+                }
+            }
+        }
+
+        // Wyświetlanie macierzy na stack panelu 
+        public void Display(StackPanel StackPanelForDisplayingAdjacencyMatrix, Canvas MyCanvas, StackPanel StackPanelForDisplayingIncidenceMatrix, StackPanel StackPanelForDisplayingAdjacencylist)
+        {
+            StackPanelForDisplayingAdjacencyMatrix.Children.Clear();
+
+            string myString = "";
+
+            for (int i = 0; i < AdjacencyArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < AdjacencyArray.GetLength(1); j++)
+                {
+                    myString += AdjacencyArray[i, j].ToString() + "  ";
+                }
+                myString += "\n";
+            }
+
+            TextBlock myBlock = new TextBlock();
+            myBlock.Text = myString;
+            myBlock.FontSize = 16;
+            StackPanelForDisplayingAdjacencyMatrix.Children.Add(myBlock);
+
+            DrawGraph(AdjacencyArray.GetLength(0), MyCanvas);
+
+            // Nowa maceirz incydencji
+            //IncidenceMatrix incidenceMatix = new IncidenceMatrix(this);
+            //incidenceMatix.Display(StackPanelForDisplayingIncidenceMatrix, StackPanelForDisplayingAdjacencylist);
+        }
+
+        // Wizualizacja grafu na macierzy
+        private void DrawGraph(int num_v, Canvas MyCanvas)
+        {
+            MyCanvas.Children.Clear();
+
+            var width = MyCanvas.Width;
+            var height = MyCanvas.Height;
+
+            Ellipse myEllipse = new Ellipse();
+            myEllipse.Height = 400;
+            myEllipse.Width = 400;
+            myEllipse.Fill = Brushes.Transparent;
+            myEllipse.StrokeThickness = 2;
+            myEllipse.Stroke = Brushes.LightGray;
+            Canvas.SetLeft(myEllipse, width / 2 - 200);
+            Canvas.SetTop(myEllipse, height / 2 - 200);
+            MyCanvas.Children.Add(myEllipse);
+
+            var r = 200;    //radius
+
+            var x_m = width / 2;    //x middle
+            var y_m = height / 2;   //y middle
+
+
+            for (int i = 1; i <= num_v; i++)
+            {
+                var angle = (2 * Math.PI) / num_v * i;
+
+                var x_oc = r * Math.Cos(angle) + x_m;   //x on cirlce
+                var y_oc = r * Math.Sin(angle) + y_m;   //y on circle
+
+                Ellipse smallPoint = new Ellipse();
+                smallPoint.Height = 8;
+                smallPoint.Width = 8;
+                smallPoint.Fill = Brushes.Black;
+                smallPoint.StrokeThickness = 1;
+                smallPoint.Stroke = Brushes.Black;
+                Canvas.SetLeft(smallPoint, x_oc - 3);
+                Canvas.SetTop(smallPoint, y_oc - 3);
+
+                TextBlock smallPointNumber = new TextBlock();
+                smallPointNumber.Text = i.ToString();
+                smallPointNumber.RenderTransform = new TranslateTransform
+                {
+                    X = (r + 10) * Math.Cos(angle) + x_m,
+                    Y = (r + 15) * Math.Sin(angle) + y_m - 8
+                };
+
+                for (int j = 1; j <= AdjacencyArray.GetLength(0); j++)
+                {
+                    if (AdjacencyArray[i - 1, j - 1] == 1)
+                    {
+                        var angle_2 = (2 * Math.PI) / num_v * j;
+
+                        var x_oc_2 = r * Math.Cos(angle_2) + x_m;   //x on cirlce
+                        var y_oc_2 = r * Math.Sin(angle_2) + y_m;   //y on circle
+
+                        // drawing arrow line
+                        Point point11 = new Point(x_oc, y_oc);
+                        Point point12 = new Point(x_oc_2, y_oc_2);
+                        DrawArrow(point11, point12, MyCanvas);
+                    }
+                }
+
+                MyCanvas.Children.Add(smallPoint);
+                MyCanvas.Children.Add(smallPointNumber);
+
+            }
+        }
+
+        private void DrawArrow(Point p1, Point p2, Canvas MyCanvas)
+        {
+            GeometryGroup lineGroup = new GeometryGroup();
+            double theta = Math.Atan2((p2.Y - p1.Y), (p2.X - p1.X)) * 180 / Math.PI;
+
+            PathGeometry pathGeometry = new PathGeometry();
+            PathFigure pathFigure = new PathFigure();
+            Point p = new Point(p1.X + ((p2.X - p1.X) / 1.35), p1.Y + ((p2.Y - p1.Y) / 1.35));
+            pathFigure.StartPoint = p;
+
+            Point lpoint = new Point(p.X + 6, p.Y + 15);
+            Point rpoint = new Point(p.X - 6, p.Y + 15);
+            LineSegment seg1 = new LineSegment();
+            seg1.Point = lpoint;
+            pathFigure.Segments.Add(seg1);
+
+            LineSegment seg2 = new LineSegment();
+            seg2.Point = rpoint;
+            pathFigure.Segments.Add(seg2);
+
+            LineSegment seg3 = new LineSegment();
+            seg3.Point = p;
+            pathFigure.Segments.Add(seg3);
+
+            pathGeometry.Figures.Add(pathFigure);
+
+            RotateTransform transform = new RotateTransform();
+            transform.Angle = theta + 90;
+            transform.CenterX = p.X;
+            transform.CenterY = p.Y;
+            pathGeometry.Transform = transform;
+            lineGroup.Children.Add(pathGeometry);
+
+            LineGeometry connectorGeometry = new LineGeometry();
+            connectorGeometry.StartPoint = p1;
+            connectorGeometry.EndPoint = p2;
+            lineGroup.Children.Add(connectorGeometry);
+
+            Path path = new Path();
+            path.Data = lineGroup;
+            path.StrokeThickness = 2;
+            path.Stroke = path.Fill = Brushes.Black;
+
+            MyCanvas.Children.Add(path);
+
+            //Label label = new Label();
+            //label.Foreground = Brushes.Black;
+            //label.Content = connection.Weight;
+            //Canvas.SetLeft(label, p.X);
+            //Canvas.SetTop(label, p.Y);
+            //_canvas.Children.Add(label);
+        }
+    }
+}
--- Grafy_4/MainWindow.xaml.cs
+++ Grafy_4/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,7 +24,51 @@ namespace Grafy_4
         public MainWindow()
         {
             InitializeComponent();
-            //aaa
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Probability_Of_Edge_Occurence.Text != "")
+            {
+                Probability_Of_Edge_Occurence.Background = Brushes.White;
+
+                //StackPanelWithConnections.Children.Clear();
+
+                Random r = new Random();
+                int v = Int32.Parse(Number_Of_Vertex.Text);
+
+                AdjacencyMatrix adjacencyMatrix = new AdjacencyMatrix(v);
+
+                for (int i = 0; i < v; i++)
+                {
+                    for (int j = 0; j < v; j++)
+                    {
+                        int probability = r.Next(0, 100);
+
+                        if (probability > Int32.Parse(Probability_Of_Edge_Occurence.Text))
+                        {
+                            adjacencyMatrix.AdjacencyArray[i, j] = 0;
+                        }
+                        else
+                        {
+                            adjacencyMatrix.AdjacencyArray[i, j] = 1;
+                        }
+                        adjacencyMatrix.AdjacencyArray[i, i] = 0;
+                    }
+                }
+
+                adjacencyMatrix.Display(StackPanelForDisplayingAdjacencyMatrix, MyCanvas, StackPanelForDisplayingIncidenceMatrix, StackPanelForDisplayingAdjacencylist);
+            }
+            else
+            {
+                Probability_Of_Edge_Occurence.Background = Brushes.OrangeRed;
+            }
+        }
+
+        private void NumbersOnly_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
